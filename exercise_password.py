6af58964cb7e3e69180ff0f24702af2d9aa785cb
estanislaodiego <ediego@mail.austral.edu.ar>
2026-03-28 16:38:35 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,12 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
-    pass
+    contrasena = input()
+    numero = ("0" in contrasena or "1" in contrasena or "2" in contrasena or "3" in contrasena or "4" in contrasena or "5" in contrasena or "6" in contrasena or "7" in contrasena or "8" in contrasena or "9" in contrasena)
+    if len(contrasena) >= 8 and numero:
+        print("Contraseña valida")
+    else:
+        if len(contrasena) < 8:
+            print("Contraseña muy corta")
+        if not numero:
+            print("Debe contener un numero")
